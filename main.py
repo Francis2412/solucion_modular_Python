959f0405a4323ae2e855b879d365762f07f44e84
Francis2412 <francisalejandrasm@gmail.com>
2026-09-18 10:39:59 -0600

    Arregle algo con la logica

--- main.py
+++ main.py
@@ -15,13 +15,13 @@ def main():
     porcentaje = porcentaje / 100
 
     #Invocar a calcular_total()
-    total, subtotal, descuento, iva = calcular_total(precio_producto1, precio_producto2, cantidad_producto1, cantidad_producto2, porcentaje, impuesto)
+    total, subtotal, descuento_producto1, descuento_producto2, iva = calcular_total(precio_producto1, precio_producto2, cantidad_producto1, cantidad_producto2, porcentaje, impuesto)
 
     #Invocar a calcular_promedio_precio()
     promedio_precio = calcular_promedio_precio(precio_producto1, precio_producto2)
 
     #invocar a mostrar factuta()
-    mostrar_factura(nombre_cliente, nombre_producto1, nombre_producto2, precio_producto1, precio_producto2, cantidad_producto1, cantidad_producto2, subtotal, descuento, iva, total, promedio_precio)
+    mostrar_factura(nombre_cliente, nombre_producto1, nombre_producto2, precio_producto1, precio_producto2, cantidad_producto1, cantidad_producto2, subtotal, iva, total, promedio_precio, descuento_producto1, descuento_producto2)
 
 def calcular_total(precio_producto1, precio_producto2, cantidad_producto1, cantidad_producto2, porcentaje, impuesto):
 
@@ -32,21 +32,29 @@ def calcular_total(precio_producto1, precio_producto2, cantidad_producto1, canti
     subtotal = calcular_total_productos (subtotal_producto1, subtotal_producto2)
 
     # Descuento
-    descuento = calcular_descuento(subtotal, porcentaje)
+    descuento_producto1, descuento_producto2 = calcular_descuento(subtotal_producto1, subtotal_producto2, porcentaje)
+    descuento = descuento_producto1 + descuento_producto2
 
     #IVA
     iva = calcular_iva(subtotal, impuesto)
     total = subtotal - descuento + iva
-    return total, subtotal, descuento, iva
+    return total, subtotal, descuento_producto1, descuento_producto2, iva
 
 def calcular_subtotal (cantidad_producto1, cantidad_producto2, precio_producto1, precio_producto2):
     subtotal_producto1 = cantidad_producto1 * precio_producto1 
     subtotal_producto2 = cantidad_producto2 * precio_producto2
     return subtotal_producto1, subtotal_producto2
 
-def calcular_descuento(subtotal, porcentaje):
-    descuento = subtotal * porcentaje
-    return descuento
+#Acumular los subtotales(
+def calcular_total_productos(subtotal_producto1, subtotal_producto2):
+    subtotal = subtotal_producto1 + subtotal_producto2 
+    return subtotal
+#)
+
+def calcular_descuento(subtotal_producto1, subtotal_producto2, porcentaje):
+    descuento_producto1 = subtotal_producto1 * porcentaje
+    descuento_producto2 = subtotal_producto2 * porcentaje
+    return descuento_producto1, descuento_producto2
 
 def calcular_iva(subtotal, impuesto):
     iva = subtotal * impuesto 
@@ -65,24 +73,24 @@ def leer_datosVentas(msj):
 
     return nombre_cliente, nombre_producto1, nombre_producto2, precio_producto1, precio_producto2, cantidad_producto1, cantidad_producto2
 
-def calcular_total_productos(subtotal_producto1, subtotal_producto2):
-    subtotal = subtotal_producto1 + subtotal_producto2 
-    return subtotal
-
 def calcular_promedio_precio(precio_producto1, precio_producto2):
     promedio_precio = (precio_producto1 + precio_producto2) / 2
     return promedio_precio
 
-def mostrar_factura (nombre_cliente, nombre_producto1, nombre_producto2, precio_producto1, precio_producto2, cantidad_producto1, cantidad_producto2, subtotal, descuento, iva, total, promedio_precio):
+def mostrar_factura (nombre_cliente, nombre_producto1, nombre_producto2, precio_producto1, precio_producto2, cantidad_producto1, cantidad_producto2, subtotal, iva, total, promedio_precio, descuento_producto1, descuento_producto2):
     os.system("cls")
 
     print("********** FACTURA **********")
     print(f"Nombre del cliente: {nombre_cliente}")
     print("Productos: ")
+  
     print(f"{nombre_producto1} x{cantidad_producto1}... {precio_producto1}")
+    print(f"Descuento del producto: {descuento_producto1}") 
+    
     print(f"{nombre_producto2} x{cantidad_producto2}... {precio_producto2}")
+    print(f"Descuento del producto: {descuento_producto2}")
+   
     print(f"Subtotal: {subtotal}")
-    print(f"Descuento: {descuento}")
     print(f"IVA: {iva}")
     print(f"Promedio de los precios: {promedio_precio}")
     print(f"Total: {total}")

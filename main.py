7f570866b4f46194d41abd4bdeadcf4b26cc568c
Francis2412 <francisalejandrasm@gmail.com>
2026-09-18 00:10:16 -0600

    Arregle muchos errores, agregue para que se limpie la pantalla e hice main

--- main.py
+++ main.py
@@ -1,20 +1,27 @@
 import os
 
 def main():
+    os.system("cls")
+
     mensaje = "Bienvenido a Comercial VAle Todo!"
-    nombre = None
-    cantidad = 0
-    precio = subtotal = descuento = iva = total = 0.0
+
     impuesto = 0.15
 
     #Invocar a leer_datosVentas()
     nombre_cliente, nombre_producto1, nombre_producto2, precio_producto1, precio_producto2, cantidad_producto1, cantidad_producto2 = leer_datosVentas(mensaje)
 
-    #Invacar a calcular_total()
+    
     porcentaje = float(input("Digite el porcentaje de descuento: "))
     porcentaje = porcentaje / 100
+
+    #Invocar a calcular_total()
     total, subtotal, descuento, iva = calcular_total(precio_producto1, precio_producto2, cantidad_producto1, cantidad_producto2, porcentaje, impuesto)
-  
+
+    #Invocar a calcular_promedio_precio()
+    promedio_precio = calcular_promedio_precio(precio_producto1, precio_producto2)
+
+    #invocar a mostrar factuta()
+    mostrar_factura(nombre_cliente, nombre_producto1, nombre_producto2, precio_producto1, precio_producto2, cantidad_producto1, cantidad_producto2, subtotal, descuento, iva, total, promedio_precio)
 
 def calcular_total(precio_producto1, precio_producto2, cantidad_producto1, cantidad_producto2, porcentaje, impuesto):
 
@@ -50,11 +57,11 @@ def leer_datosVentas(msj):
     print("*"*40)
     nombre_cliente = input("Digite el nombre del cliente: ")
     nombre_producto1 = input("Ingrese el nombre del producto #1: ")
-    precio_producto1 = float(input("Ingrese el precio del producto #1"))
-    cantidad_producto1 = float(input("Ingrese la cantidad del producto #1"))
+    precio_producto1 = float(input("Ingrese el precio del producto #1: "))
+    cantidad_producto1 = int(input("Ingrese la cantidad del producto #1: "))
     nombre_producto2 = input("Ingrese el nombre del producto #2: ")
-    precio_producto2 = float(input("Ingrese el precio del producto #2"))
-    cantidad_producto2 = float(input("Ingrese la cantidad del producto #2"))
+    precio_producto2 = float(input("Ingrese el precio del producto #2: "))
+    cantidad_producto2 = int(input("Ingrese la cantidad del producto #2: "))
 
     return nombre_cliente, nombre_producto1, nombre_producto2, precio_producto1, precio_producto2, cantidad_producto1, cantidad_producto2
 
@@ -67,7 +74,9 @@ def calcular_promedio_precio(precio_producto1, precio_producto2):
     return promedio_precio
 
 def mostrar_factura (nombre_cliente, nombre_producto1, nombre_producto2, precio_producto1, precio_producto2, cantidad_producto1, cantidad_producto2, subtotal, descuento, iva, total, promedio_precio):
-    print("************** FACTURA **************")
+    os.system("cls")
+
+    print("********** FACTURA **********")
     print(f"Nombre del cliente: {nombre_cliente}")
     print("Productos: ")
     print(f"{nombre_producto1} x{cantidad_producto1}... {precio_producto1}")
